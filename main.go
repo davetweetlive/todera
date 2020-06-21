5e7ed126ffded1a50021a257af01a84701090680
Dave Augustus <davetweetlive@gmail.com>
2020-06-22 01:44:38 +0530

    Working on mysql

--- main.go
+++ main.go
@@ -1,15 +1,26 @@
 package main
 
 import (
+	"Blog/models"
 	"log"
 	"net/http"
 )
 
+// var SQLConnector *sql.DB
+
+// func init() {
+// 	// Connect to the database
+// 	SQLConnector, _ = models.EstablishDBConnection()
+// }
+
 func main() {
+	models.CreateTable()
+	// fmt.Println(SQLConnector)
 	// To serve static fils
 	fileServer := http.FileServer(http.Dir("static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fileServer))
 
+	// different handlers
 	http.HandleFunc("/", LandingPageHandler)
 	http.HandleFunc("/login", loginHandler)
 	http.HandleFunc("/signup", SignupHandler)
---
+++ models/db.go
@@ -0,0 +1,61 @@
+package models
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strconv"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type DBStruct struct {
+	Db Mysqldb `json:"mysqldb"`
+}
+
+type Mysqldb struct {
+	Name      string `json:"name"`
+	Connector string `json:"connector"`
+	Host      string `json:"host"`
+	Port      int    `json:"port"`
+	Database  string `json:"database"`
+	Username  string `json:"username"`
+	Password  string `json:"password"`
+}
+
+func SqlConnectionString() string {
+
+	jsonFile, err := os.Open("config/dbcred.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// fmt.Println("Successfully Opened dbcred.json")
+	// defer the closing of our jsonFile so that we can parse it later on
+	defer jsonFile.Close()
+
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+
+	// we initialize our dbStruct struct
+	var dbStruct DBStruct
+
+	json.Unmarshal(byteValue, &dbStruct)
+
+	dbConnString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbStruct.Db.Username, dbStruct.Db.Password, dbStruct.Db.Host, strconv.Itoa(dbStruct.Db.Port), dbStruct.Db.Database)
+
+	return dbConnString
+}
+
+func EstablishDBConnection() (*sql.DB, error) {
+
+	db, err := sql.Open("mysql", SqlConnectionString())
+
+	if err != nil {
+		log.Fatal("Couldn't connect to the database!")
+	}
+
+	return db, err
+}
